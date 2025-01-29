5dfcd9abbe9a5f347229fc1b7e9d3e12c0d3a322
aisha patel <aishapatel0703@gmail.com>
2025-01-29 00:43:55 -0500

    pushing everything i finished for CHAPTER 4

--- app/src/main/java/com/example/andriodchapterprojects/MainActivity.java
+++ app/src/main/java/com/example/andriodchapterprojects/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.andriodchapterprojects;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +26,91 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        initListButton();
+        initMapButton();
+        initSettingsButton();
+        initToggleButton();
+        setForEditing(false);
+    }
+    private void initListButton(){
+        ImageButton ibList=findViewById(R.id.contactslistbutton);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, ContactListActivty.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+    private void initMapButton(){
+        ImageButton ibList=findViewById(R.id.mapsicon);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, MapActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+    private void initSettingsButton(){
+        ImageButton ibList=findViewById(R.id.settingsicon);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, SettingsActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+    private void initToggleButton(){
+        final ToggleButton edit=(ToggleButton)findViewById(R.id.editbutton);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setForEditing(edit.isChecked());
+            }
+        });
+    }
+    private void setForEditing(boolean enabled){
+        EditText editName=findViewById(R.id.editName);
+        EditText editAddress=findViewById(R.id.editAddress);
+        EditText editCity=findViewById(R.id.editCity);
+        EditText editState=findViewById(R.id.editState);
+        EditText editZipcode=findViewById(R.id.editZipcode);
+        EditText phone=findViewById(R.id.editHome);
+        EditText cell=findViewById(R.id.editCell);
+        EditText editEmail=findViewById(R.id.editEmail);
+        Button buttonchange=findViewById(R.id.btnBirthday);
+        Button buttonsave=findViewById(R.id.savebutton);
+
+        editName.setEnabled(enabled);
+        editAddress.setEnabled(enabled);
+        editCity.setEnabled(enabled);
+        editState.setEnabled(enabled);
+        editZipcode.setEnabled(enabled);
+        phone.setEnabled(enabled);
+        cell.setEnabled(enabled);
+        editEmail.setEnabled(enabled);
+        buttonchange.setEnabled(enabled);
+        buttonsave.setEnabled(enabled);
+        if(enabled){
+            editName.requestFocus();
+        }
+
+    }
+    private void initChangeDateButton(){
+        Button changeDate=findViewById(R.id.btnBirthday);
+        changeDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm=getSupportFragmentManager();
+                DatePickerDialog datePickerDialog=new DatePickerDialog();
+
+            }
         });
     }
 }
