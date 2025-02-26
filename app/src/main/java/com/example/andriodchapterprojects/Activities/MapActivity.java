b13a7ee5b2313c97dae68e70dfbb48d008a4f500
aishaapat <aishapatel0703@gmail.com>
2025-02-26 09:37:39 -0500

    I think I got the packages to work, I am at work I will be working on this later today to wrap up hopefully

--- app/src/main/java/com/example/andriodchapterprojects/Activities/MapActivity.java
+++ app/src/main/java/com/example/andriodchapterprojects/Activities/MapActivity.java
@@ -1,6 +1,9 @@
 package com.example.andriodchapterprojects.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.andriodchapterprojects.R;
+
+import java.util.Map;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -22,5 +27,31 @@ public class MapActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initListButton();
+        initSettingsButton();
     }
+    public void initListButton(){
+        ImageButton ibList=findViewById(R.id.contactslistbutton);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MapActivity.this, ContactListActivty.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+    public void initSettingsButton(){
+        ImageButton ibList=findViewById(R.id.settingsicon);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(MapActivity.this, SettingsActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
