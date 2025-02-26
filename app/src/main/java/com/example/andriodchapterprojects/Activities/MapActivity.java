e6deb7d50a5b855e2fcdc7265265845b1a6bf062
aishaapat <aishapatel0703@gmail.com>
2025-02-26 14:33:42 -0500

    had to add one more dependecy and the create() method is depracted so I have to add the new version

--- app/src/main/java/com/example/andriodchapterprojects/Activities/MapActivity.java
+++ app/src/main/java/com/example/andriodchapterprojects/Activities/MapActivity.java
@@ -1,21 +1,67 @@
 package com.example.andriodchapterprojects.Activities;
 
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.andriodchapterprojects.DB.Contact;
 import com.example.andriodchapterprojects.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.snackbar.Snackbar;
 
-import java.util.Map;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MapActivity extends AppCompatActivity {
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    final int PERMISSION_REQUEST_LOCATION = 101;
+    GoogleMap gmap;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationRequest locationRequest;
+    LocationCallback locationCallback;
+
+    ArrayList<Contact> contacts = new ArrayList<>();
+    Contact currentContact = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +75,16 @@ public class MapActivity extends AppCompatActivity {
         });
         initListButton();
         initSettingsButton();
+        SupportMapFragment mapFragment=(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        createLocationRequest();
+        createLocationCallback();
+
+
     }
+
 
     public void initListButton(){
         ImageButton ibList=findViewById(R.id.contactslistbutton);
@@ -54,5 +109,22 @@ public class MapActivity extends AppCompatActivity {
             }
         });
     }
+    private void createLocationRequest(){
+        locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 10000)
+                .setMinUpdateIntervalMillis(10000)
+                .build();
+    }
 
+    private void startLocationUpdates(){
+        if(Build.VERSION.SDK_INT>=23 && ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED){
+
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
 }
