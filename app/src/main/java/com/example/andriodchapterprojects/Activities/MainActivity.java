5c857f3659508faea946d63d1c76414665c1ede8
aishaapat <aishapatel0703@gmail.com>
2025-03-03 18:07:04 -0500

    8.6-8.9

--- app/src/main/java/com/example/andriodchapterprojects/Activities/MainActivity.java
+++ app/src/main/java/com/example/andriodchapterprojects/Activities/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.andriodchapterprojects.Activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.text.Editable;
@@ -16,6 +20,8 @@ import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -24,12 +30,14 @@ import com.example.andriodchapterprojects.DB.Contact;
 import com.example.andriodchapterprojects.DB.ContactDataSource;
 import com.example.andriodchapterprojects.Fragments.DatePickerDialog;
 import com.example.andriodchapterprojects.R;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.sql.SQLException;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity implements DatePickerDialog.SaveDateListener {
     private Contact currentContact;
+    final int PERMISSION_REQUEST_PHONE=102;
 
 
     @Override
@@ -37,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_main_page), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -53,7 +61,32 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         initChangeDateButton();
         initTextChangedEvents();
         initSaveButton();
+        initCallFunction();
 
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CALL_PHONE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            return ;
+        }
+
+    }
+    @Override
+    public void onRequestPermissionsResult (int requestCode, String permissions[], int[] grantResults){
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case PERMISSION_REQUEST_PHONE: {
+                if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(MainActivity.this,"You may now call from this app",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this,"You will not bee able to call",Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
     public void initListButton(){
         ImageButton ibList=findViewById(R.id.contactslistbutton);
@@ -361,5 +394,57 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         birth.setText(DateFormat.format("MM/dd/yyyy",currentContact.getBirthday().getTimeInMillis()).toString());
     }
 
+    private void initCallFunction(){
+        EditText editPhone=(EditText) findViewById(R.id.editHome);
+        editPhone.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                checkPhonePermission(currentContact.getPhoneNumber());
+                return false;
+            }
+        });
+        EditText editCell=(EditText) findViewById(R.id.editCell);
+        editCell.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                checkPhonePermission(currentContact.getCellNumber());
+                return false;
+            }
+        });
+    }
+
+    private void checkPhonePermission(String phoneNumber){
+        if(Build.VERSION.SDK_INT>=23)
+        {
+            if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+            {
+                if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.CALL_PHONE))
+                {
+                    Snackbar.make(findViewById(R.id.activity_main_page),"MyContactList requires this permission to place a call",Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CALL_PHONE},
+                                    PERMISSION_REQUEST_PHONE);
+                        }
+                    }).show();
+                }
+                else
+                { callContact(phoneNumber);
+                }
+            }
+            else{
+                callContact(phoneNumber);
+            }
+        }
+    }
+
+    private void callContact(String phoneNumber){
+        Intent intent=new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:"+phoneNumber));
+        if (Build.VERSION.SDK_INT>=23 && ContextCompat.checkSelfPermission(getBaseContext(),Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+        else startActivity(intent);
+    }
 
 }
